44569ab688d7c6bf59b59c0f3e208b04d7f8df57
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-01-19 18:06:40 +0200

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import style from './Footer.module.css';
+
+function Templates() {
+    const [state, setstate] = useState();
+    useEffect(() => {
+        
+        return () => {
+            
+        };
+    }, []);
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Templates;
