5e827a6d809b934efd2f13c2d0312cbf4cfd9604
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-01-20 16:19:37 +0200

    add Responsive Design to navbar Component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import style from './Navbar.module.css';
+import { NavLink } from 'react-router-dom';
 
 function Templates() {
-    const [state, setstate] = useState();
+    const [isScroll, setIsScroll] = useState(false);
+    function handleScroll (){
+        if(window.scrollY > 10){
+            setIsScroll(true);
+        }else {
+            setIsScroll(false);
+        }
+    }
+    window.addEventListener("scroll", handleScroll);
     useEffect(() => {
         
         return () => {
@@ -11,8 +20,20 @@ function Templates() {
     }, []);
 
     return (
-        <div>
-            
+        <div className={`navbar w-full bg-slate-400 fixed top-0 ${isScroll ? 'py-4' : 'py-8'}`}>
+            <div className='container mx-auto px-4 flex justify-between'>
+                <h1 className='flex gap-1 items-center'><i className="fa-solid fa-cart-plus"></i>FreshCart</h1>
+                <button className='block sm:hidden'><i className="fa-solid fa-bars"></i></button>
+                <div className='hidden sm:block'>
+                    <ul className='flex gap-5'>
+                        <li><NavLink>Home</NavLink></li>
+                        <li><NavLink>Products</NavLink></li>
+                        <li><NavLink>Brands</NavLink></li>
+                        <li><NavLink>Categories</NavLink></li>
+                        <li><NavLink>Cart</NavLink></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
