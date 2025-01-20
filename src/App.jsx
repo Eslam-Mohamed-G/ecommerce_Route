4a01c49a57b5a607b214d393b518a6fd02100965
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-01-20 14:34:15 +0200

    add Register Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Home from './components/Register/Register.jsx';
 import Layout from './components/Layout/Layout.jsx';
 import Home from './components/Home/Home.jsx';
 import './App.css';
@@ -6,13 +7,17 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 function App() {
   const routes = createBrowserRouter([
-    {path:'/', element:<Layout/>, children:[{
-      index:true, element:<Home/>
-    }]}
+    {
+      path: '/', element: <Layout />,
+      children: [
+        { index: true, element: <Home /> },
+        { path:'register', element: <Register/>}
+      ]
+    }
   ])
 
   return (
-    <RouterProvider router = {routes}/>
+    <RouterProvider router={routes} />
   )
 }
 
---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import style from './Register.module.css';
+
+function Templates() {
+    const [state, setstate] = useState();
+    useEffect(() => {
+        
+        return () => {
+            
+        };
+    }, []);
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Templates;
