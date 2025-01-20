51cf2763eb8e20c65e94ca57d2cc0834c27a9504
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-01-20 16:55:38 +0200

    fetch data form api by axios

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import style from './Home.module.css';
+import axios from "axios";
 
 function Templates() {
-    const [state, setstate] = useState();
+    const [products, setProducts] = useState(null);
+    async function getAllProducts (){
+        try {
+            const {data} = await axios.get(`https://ecommerce.routemisr.com/api/v1/products`);
+            setProducts(data.data)
+            console.log(data.data);
+        } catch (error) {
+            console.error('error all products', error);
+        }
+    }
     useEffect(() => {
-        
+        getAllProducts()
         return () => {
             
         };
