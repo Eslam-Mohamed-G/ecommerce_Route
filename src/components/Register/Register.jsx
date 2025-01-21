1911ae33782648b9ecf86aa0aa95b8ad7f3d3e3f
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-01-21 22:57:04 +0200

    add style to form

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -38,11 +38,13 @@ function Register() {
         };
     }, []);
 
-    const trueMessage = "border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600";
-    const falseMessage = "text-red-600 border-red-300 appearance-none dark:text-white dark:border-red-600 dark:focus:border-red-500 focus:outline-none focus:ring-0 focus:border-red-600";
+    // const trueMessage = "border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600";
+    const inputGray = "border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 min-w-0 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500";
+    const trueMessage = "border-green-500 text-green-500 appearance-none dark:text-green-400 focus:ring-green-500 focus:border-green-500 dark:bg-gray-700 dark:border-green-500";
+    const falseMessage = "border-red-500 text-red-500 appearance-none text-sm focus:ring-red-500 dark:bg-gray-700 focus:border-red-500 dark:text-red-500 dark:border-red-500";
 
     return (
-        <div>
+        <div className='max-w-lg p-6 m-auto bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
             <form onSubmit={formik.handleSubmit} className="max-w-md mx-auto">
                 <div className="relative z-0 w-full mb-6 group">
                     <input 
@@ -52,7 +54,7 @@ function Register() {
                         onBlur={formik.handleBlur} 
                         name="name" 
                         id="name" 
-                        className={`block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 ${ formik.touched.name ? formik.errors.name ? falseMessage : trueMessage : trueMessage} peer`} 
+                        className={`block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 focus:outline-none ${ formik.touched.name ? formik.errors.name ? falseMessage : trueMessage : inputGray} peer`} 
                         placeholder=" " 
                     />
                     <label htmlFor="name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Name</label>
@@ -74,7 +76,7 @@ function Register() {
                         onBlur={formik.handleBlur}
                         name="email" 
                         id="email" 
-                        className={`block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 ${ formik.touched.email ? formik.errors.email ? falseMessage : trueMessage : trueMessage} peer`} 
+                        className={`block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 focus:outline-none ${ formik.touched.email ? formik.errors.email ? falseMessage : trueMessage : inputGray} peer`} 
                         placeholder=" " 
                     />
                     <label htmlFor="email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
@@ -97,7 +99,7 @@ function Register() {
                         onBlur={formik.handleBlur}
                         name="password" 
                         id="password" 
-                        className={`block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 ${ formik.touched.password ? formik.errors.password ? falseMessage : trueMessage : trueMessage} peer`} 
+                        className={`block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 focus:outline-none ${ formik.touched.password ? formik.errors.password ? falseMessage : trueMessage : inputGray} peer`} 
                         placeholder=" " 
                     />
                     <label htmlFor="password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
@@ -120,7 +122,7 @@ function Register() {
                         onBlur={formik.handleBlur}
                         name="rePassword" 
                         id="rePassword" 
-                        className={`block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 ${ formik.touched.rePassword ? formik.errors.rePassword ? falseMessage : trueMessage : trueMessage} peer`} 
+                        className={`block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 focus:outline-none ${ formik.touched.rePassword ? formik.errors.rePassword ? falseMessage : trueMessage : inputGray} peer`} 
                         placeholder=" " 
                     />
                     <label htmlFor="rePassword" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirm password</label>
@@ -143,10 +145,10 @@ function Register() {
                         onBlur={formik.handleBlur}
                         name="phone" 
                         id="phone" 
-                        className={`block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 ${ formik.touched.rePassword ? formik.errors.rePassword ? falseMessage : trueMessage : trueMessage} peer`} 
+                        className={`block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 focus:outline-none ${ formik.touched.phone ? formik.errors.phone ? falseMessage : trueMessage : inputGray} peer`} 
                         placeholder=" "
                     />
-                    <label htmlFor="phone" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">phone</label>
+                    <label htmlFor="phone" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-white duration-300 transform -translate-y-6 scale-75 top-3 z-50 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">phone</label>
                     {formik.errors.phone && formik.touched.phone ?
                         <>
                             <i className="absolute top-1/3 end-0 text-red-600 fa-solid fa-xmark"></i>
@@ -159,7 +161,7 @@ function Register() {
 
                 <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
                 
-                <div className="relative z-0 w-full mb-6 group border text-center">
+                <div className={`relative z-0 w-full mb-6 group text-center ${messageFromBackEnd ? '' : 'hidden' }`}>
                     {messageFromBackEnd ? <p className='text-red-500'>{messageFromBackEnd}</p> : <p className='text-green-500'>succeed</p>}
                 </div>
             </form>
