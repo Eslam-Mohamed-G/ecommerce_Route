d3a2d039d61018ab6284f70744a38115353eaa1e
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-01-22 09:47:31 +0200

    add form to Login Component

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -17,7 +17,7 @@ function Login() {
             email : '',
             password : ''
         },
-        
+
         validationSchema:validator,
 
         onSubmit: (values)=>{
@@ -39,6 +39,11 @@ function Login() {
         };
     }, []);
 
+    const inputGray = "border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 min-w-0 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500";
+    const trueMessage = "border-green-500 text-green-500 appearance-none dark:text-green-400 focus:ring-green-500 focus:border-green-500 dark:bg-gray-700 dark:border-green-500";
+    const falseMessage = "border-red-500 text-red-500 appearance-none text-sm focus:ring-red-500 dark:bg-gray-700 focus:border-red-500 dark:text-red-500 dark:border-red-500";
+
+
     return ( 
         <div className="w-full py-4 sm:py-6 md:py-8 dark:bg-gray-800 dark:border-gray-700">
             <div className="relative py-3 max-w-lg sm:max-w-xl sm:mx-auto">
@@ -51,13 +56,16 @@ function Login() {
                         <div className="divide-y divide-gray-200">
                             <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                                 {/* email   email   email */}
-                                <div className="relative">
+                                <div className="relative mb-8">
                                     <input 
+                                        value={formik.values.email}
+                                        onChange={formik.handleChange}
+                                        onBlur={formik.handleBlur}
                                         autoComplete="off" 
                                         id="email" 
                                         name="email" 
                                         type="email"
-                                        className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                                        className={`peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600 ${formik.touched.email ? formik.errors.email ? falseMessage : trueMessage : inputGray}`}
                                         placeholder="Email address" 
                                     />
                                     <label 
@@ -65,19 +73,39 @@ function Login() {
                                         className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email
                                         Address
                                     </label>
+
+                                    {formik.errors.email && formik.touched.email ?
+                                        <>
+                                            <i className="absolute top-1/3 end-0 text-red-600 fa-solid fa-xmark"></i>
+                                            <p className='absolute -bottom-5 text-red-600 text-[12px]'>{formik.errors.email}</p>
+                                        </>
+                                        :
+                                        <i className={`${formik.touched.email ? formik.errors.email ? 'hidden' : '' : 'hidden'} absolute top-1/3 end-0 text-green-500 fa-solid fa-check`}></i>
+                                    }
                                 </div>
 
                                 {/* password   password   password */}
-                                <div className="relative">
+                                <div className="relative border">
                                     <input 
+                                        value={formik.values.password}
+                                        onChange={formik.handleChange}
+                                        onBlur={formik.handleBlur}
                                         autoComplete="off" 
                                         id="password" 
                                         name="password" 
                                         type="password" 
-                                        className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" 
+                                        className={`peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600 ${formik.touched.password ? formik.errors.password ? falseMessage : trueMessage : inputGray}`} 
                                         placeholder="Password" 
                                     />
                                     <label htmlFor="password" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
+                                    {formik.errors.password && formik.touched.password ?
+                                        <>
+                                            <i className="absolute top-1/3 end-7 text-red-600 fa-solid fa-xmark"></i>
+                                            <p className='absolute -bottom-5 text-red-600 text-[12px]'>{formik.errors.password}</p>
+                                        </>
+                                        :
+                                        <i className={`${formik.touched.password ? formik.errors.password ? 'hidden' : '' : 'hidden'} absolute top-1/3 end-0 text-green-500 fa-solid fa-check`}></i>
+                                    }
                                     <div className='absolute top-2 end-0'>
                                         <i className="fa-regular fa-eye"></i>
                                         <i className="fa-regular fa-eye-slash"></i>
