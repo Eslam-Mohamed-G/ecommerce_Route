d0fff37be5f2f06488bb504eaaac313852f24200
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-01-22 08:34:54 +0200

    add form to Login Component

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -67,7 +67,7 @@ function Login() {
                                 {/* Login with Google */}
                                 <button type="button" className="w-full block bg-white hover:bg-gray-100 focus:bg-gray-100 text-gray-900 font-semibold rounded-lg px-4 py-3 border border-gray-300">
                                     <div className="flex items-center justify-center">
-                                        <img src={Google_logo} alt="Google_logo" className='w-8'/>
+                                        <img src={Google_logo} alt="Google_logo" className='w-7'/>
                                         <span className="ml-2">Login with Google</span>
                                     </div>
                                 </button>
