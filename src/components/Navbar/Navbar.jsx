21f93821144f35243592147167812422d3ac838a
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-01-22 16:18:09 +0200

    add new style to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -20,15 +20,22 @@ function Templates() {
     }, []);
 
     return (
-        <div className={`navbar w-full backdrop-blur-sm bg-slate-400/30 fixed top-0 transition-all ease-in-out duration-500 z-50 ${isScroll ? 'py-4' : 'py-8'} mx-auto px-4 sm:px-8`}>
-            <div className='container mx-auto px-4 flex justify-between'>
-                <h1 className='flex gap-1 items-center'><i className="fa-solid fa-cart-plus"></i>FreshCart</h1>
+            <>
+        <div className={`navbar w-full backdrop-blur-sm fixed top-0 transition-all ease-in-out duration-500 z-50`}>
+            <div className="w-full bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform">
+                <div className='container mx-auto px-8 sm:px-12 py-2 flex justify-between'>
+                    <h1 className='block text-lg font-bold'><span className='flex gap-1 items-center'><i className="fa-solid fa-cart-plus"></i>FreshCart</span></h1>
+                    <ul className='flex gap-5'>
+                        <li><NavLink to={'/register'}>Register</NavLink></li>
+                        <li><NavLink to={'/login'}>Login</NavLink></li>
+                    </ul>
+                </div>
+            </div>
+            <div className={`container mx-auto px-8 sm:px-12 bg-slate-400/30 flex justify-between align-middle ${isScroll ? 'py-3' : 'py-5'}`}>
                 <button className='block sm:hidden'><i className="fa-solid fa-bars"></i></button>
                 <div className='hidden sm:block'>
                     <ul className='flex gap-5'>
                         <li><NavLink to={'/'}>Home</NavLink></li>
-                        <li><NavLink to={'/register'}>Register</NavLink></li>
-                        <li><NavLink to={'/login'}>Login</NavLink></li>
                         <li><NavLink>Products</NavLink></li>
                         <li><NavLink>Brands</NavLink></li>
                         <li><NavLink>Categories</NavLink></li>
@@ -37,6 +44,7 @@ function Templates() {
                 </div>
             </div>
         </div>
+        </>
     )
 }
 
