ac8ad3a413ef5767c462d47405807555c38fe894
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-01-22 07:03:13 +0200

    add form to Login Component

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import style from './Login.module.css';
 
-function Templates() {
+function Login() {
     const [state, setstate] = useState();
     useEffect(() => {
 
@@ -16,22 +16,34 @@ function Templates() {
                 <div className="mb-5">
                     <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
                         email</label>
-                    <input type="email" id="email"
+                    <input 
+                        type="email" 
+                        id="email"
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="name@flowbite.com" required />
+                        placeholder="name@flowbite.com" 
+                        required 
+                    />
                 </div>
+                
                 <div className="mb-5">
                     <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
                         password</label>
-                    <input type="password" id="password"
+                    <input 
+                        type="password" 
+                        id="password"
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        required />
+                        required
+                    />
                 </div>
                 <div className="flex items-start mb-5">
                     <div className="flex items-center h-5">
-                        <input id="remember" type="checkbox" defaultValue
+                        <input 
+                            id="remember" 
+                            type="checkbox" 
+                            defaultValue
                             className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                            required />
+                            required 
+                        />
                     </div>
                     <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember
                         me</label>
@@ -43,4 +55,4 @@ function Templates() {
     )
 }
 
-export default Templates;
+export default Login;
