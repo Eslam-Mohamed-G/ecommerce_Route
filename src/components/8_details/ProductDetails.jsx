757f81889ba8d819e72c4d8fc6fcd30305e879ef
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-02-12 21:17:46 +0200

    Add style to details

--- src/components/8_details/ProductDetails.jsx
+++ src/components/8_details/ProductDetails.jsx
@@ -38,23 +38,22 @@ function ProductDetails() {
             
             {!loading && !errorMSG && (
                 <>
-                    <div className='row flex flex-wrap gap-5 justify-center sm:justify-start'>
-                        <div className='card w-full sm:w-fit max-w-sm overflow-hidden p-6 pt-3 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700'>
-                            <figure className='w-56 h-56 mx-auto mb-6 rounded-t-lg overflow-hidden'>
-                                <img src={details?.imageCover} alt="product-image" className='w-56 h-56 object-cover' />
+                    <div className='row flex flex-wrap justify-center sm:justify-start m-auto'>
+                        <div className='card w-full sm:w-1/2 max-w-sm overflow-hidden bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700'>
+                            <figure className='w-72 h-72 sm:w-96 sm:h-96 mx-auto rounded-t-lg overflow-hidden'>
+                                <img src={details?.imageCover} alt="product-image" className='w-full' />
                             </figure>
-                            <div className='flex flex-wrap justify-between mt-4 md:mt-6'>
+                            <div className='flex flex-wrap justify-between mt-4 md:mt-6 px-6 py-3'>
                                 <button onClick={() => addToCart(details._id)} className='inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>add to cart</button>
                                 <button onClick={()=>{postWishlist(details._id)}}><span className='text-[24px]'><i className="fa-solid fa-heart"></i></span></button>
                             </div>
                         </div>
-                        <div className='card p-6'>
+                        <div className='card sm:w-1/2 pt-6 sm:p-4'>
                             <h1 className='text-lg font-bold'>{details?.title}</h1>
                             <div className="flex items-center gap-10 mt-2.5 mb-5">
                                 <h2 className='flex gap-4'> <p className={`relative ${details?.priceAfterDiscount ? 'after:w-full after:h-[2px] after:bg-red-400 after:absolute after:start-0 after:top-1/2' : ''}`}>{details?.price} EGP</p> <p>{details?.priceAfterDiscount ? `${details?.priceAfterDiscount} EGP` : ""}</p></h2>
                                 <div className="flex items-center space-x-1 rtl:space-x-reverse">
-                                    <div className="text-yellow-400">★★★★</div>
-                                    <div className="text-gray-300">★</div>
+                                    <div className="text-yellow-400">★★★★ <span className="text-gray-300">★</span></div>
                                     <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded-sm dark:bg-blue-200 dark:text-blue-800 ms-3">{details.ratingsAverage}</span>
                                 </div>
                             </div>
