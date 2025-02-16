c2708ca8517c64163af9262490d79a448cbe84eb
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-02-16 21:00:38 +0200

    add footer Component

---
+++ src/components/12_Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+
+    return (
+        <div className='container mx-auto mt-8 px-4 sm:px-12 py-4 text-center bg-slate-400/30'>
+            <p className='text-lg'>© 2025 <a href="https://github.com/Eslam-Mohamed-G" target='blank' className='bg-gradient-to-r from-blue-300 to-blue-600 bg-clip-text text-transparent underline decoration-inherit'>Eslam Mohamed</a>. All Rights Reserved.</p>
+        </div>
+    )
+}
+
+export default Footer;
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from '../1_Navbar/Navbar.jsx';
-import Footer from '../6_Footer/Footer.jsx';
+import Footer from '../12_Footer/Footer.jsx';
 import { Outlet } from 'react-router-dom';
 import style from './Layout.module.css';
 import { Toaster } from 'react-hot-toast';
@@ -17,7 +17,7 @@ function Templates() {
     return (
         <div>
             <Navbar/>
-            <div className='min-h-dvh overflow-hidden mx-auto pt-[108px] dark:bg-slate-950'>
+            <div className='min-h-dvh overflow-hidden mx-auto pt-[108px] pb-8 dark:bg-slate-950'>
                 <Outlet/>
                 <Toaster/>
             </div>
