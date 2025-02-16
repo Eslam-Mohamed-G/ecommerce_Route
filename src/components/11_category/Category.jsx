b7c737ba786edac14db8f70bda02695a809b7b04
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-02-16 18:46:07 +0200

    add Categort Component

--- src/components/11_category/Category.jsx
+++ src/components/11_category/Category.jsx
@@ -16,23 +16,20 @@ function Category() {
             {loading && <Loading />}
             {errorMSG && <p className='text-center capitalize text-red-500 font-sans font-bold text-[34px]'>{errorMSG}</p>}
             {!loading && !errorMSG && (
-                <>
-                    <h1 className='ps-2 pb-2'>shop Popular Categories</h1>
-                    <div className='flex flex-row flex-wrap w-full'>
-                        {categories?.map((element) => (
-                            <div key={element._id} data-aos="zoom-in" className='w-1/2 sm:w-1/4 p-2 '>
-                                <div className='border border-blue-500 rounded overflow-hidden flex flex-col'>
-                                    <div className='relative'>
-                                        <img src={element.image} alt={element.name} className='block w-full h-36 sm:h-72 object-cover' />
-                                    </div>
-                                    <div className='bg-blue-100 grow'>
-                                        <h4 className='text-center'>{element.name}</h4>
-                                    </div>
+                <div className='flex flex-row flex-wrap w-full'>
+                    {categories?.map((element) => (
+                        <div key={element._id} data-aos="zoom-in" className='w-1/2 sm:w-1/4 p-2 '>
+                            <div className='border border-blue-500 rounded overflow-hidden flex flex-col'>
+                                <div className='relative'>
+                                    <img src={element.image} alt={element.name} className='block w-full h-36 sm:h-72 object-cover' />
+                                </div>
+                                <div className='bg-blue-100 grow'>
+                                    <h4 className='text-center'>{element.name}</h4>
                                 </div>
                             </div>
-                        ))}
-                    </div>
-                </>
+                        </div>
+                    ))}
+                </div>
             )}
         </div>
     )
